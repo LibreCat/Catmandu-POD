a573bd5f317f33eea5792efa3db78128604ecf45
Nicolas Franck <nicolas.franck@ugent.be>
2014-10-20 14:22:41 +0200

    milla setup

---
+++ cpanfile
@@ -0,0 +1,12 @@
+#requirements
+requires 'perl','5.10.1';
+requires 'Catmandu::Sane','0';
+requires 'Moo','0';
+requires 'Catmandu::Fix::Has','0';
+requires 'Pod::Tree','0';
+requires 'File::Spec','0';
+
+on 'test', sub {
+    requires 'Test::Exception','0';
+    requires 'Test::More','0';
+};
---
+++ lib/Catmandu/POD.pm
@@ -0,0 +1,24 @@
+package Catmandu::POD;
+our $VERSION = "0.01";
+
+=head1 NAME
+
+Catmandu::POD - Catmandu modules for working with pod files
+
+=head1 MODULES
+
+L<Catmandu::Fix::pod_tag>
+
+L<Catmandu::Cmd::syntax_info>
+
+=head1 AUTHOR
+
+Nicolas Franck C<< <nicolas.franck at ugent.be> >>
+
+=head1 SEE ALSO
+
+L<Catmandu::Fix>,L<Catmandu::Cmd>
+
+=cut
+
+1;
